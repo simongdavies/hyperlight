302ac09126ad44122cc0087f496bb76991467514
danbugs <danilochiarlone@gmail.com>
2025-05-15 19:34:06 -0700

    [dev/verify-msrv.sh] use 'x86_64-unknown-none' target for guest lib
    Now, because we no longer support building the guest library for MSVC, we directly
    specify to use 'x86_64-unknown-none' for it in the script. Otherwise, on Windows,
    as is, we'd try to use MSVC.
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- dev/verify-msrv.sh
+++ dev/verify-msrv.sh
@@ -6,10 +6,21 @@ for CRATE in "$@"; do
     if ! rustup toolchain list | grep -q "$VERSION"; then
         rustup --quiet toolchain install "$VERSION" --no-self-update --profile minimal
     fi
-    if cargo +$VERSION check --quiet -p $CRATE; then
-        echo -e " \u001b[1;32m✓\u001b[0m Crate \u001b[1m$CRATE\u001b[0m builds with rust \u001b[1m$VERSION\u001b[0m"
+    if [[ "$CRATE" == "hyperlight-guest" ]]; then
+        TARGET="x86_64-unknown-none"
+        rustup target add "$TARGET" --toolchain "$VERSION" >/dev/null
+        if cargo +"$VERSION" check --quiet -p "$CRATE" --target "$TARGET"; then
+            echo -e " \u001b[1;32m✓\u001b[0m Crate \u001b[1m$CRATE\u001b[0m builds with rust \u001b[1m$VERSION\u001b[0m and target \u001b[1m$TARGET\u001b[0m"
+        else
+            echo -e " \u001b[1;31m✗\u001b[0m Crate \u001b[1m$CRATE\u001b[0m fails with rust \u001b[1m$VERSION\u001b[0m and target \u001b[1m$TARGET\u001b[0m"
+            exit 1
+        fi
     else
-        echo -e " \u001b[1;31m✗\u001b[0m Crate \u001b[1m$CRATE\u001b[0m fails with rust \u001b[1m$VERSION\u001b[0m"
-        exit 1
+        if cargo +"$VERSION" check --quiet -p "$CRATE"; then
+            echo -e " \u001b[1;32m✓\u001b[0m Crate \u001b[1m$CRATE\u001b[0m builds with rust \u001b[1m$VERSION\u001b[0m"
+        else
+            echo -e " \u001b[1;31m✗\u001b[0m Crate \u001b[1m$CRATE\u001b[0m fails with rust \u001b[1m$VERSION\u001b[0m"
+            exit 1
+        fi
     fi
 done
