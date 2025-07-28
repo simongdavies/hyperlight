7e4517c4606537ff121288a653b8eb1be1c4d657
Simon Davies <simongdavies@users.noreply.github.com>
2025-07-28 22:24:20 +0100

    Remove mem_access_handler_wrapper function
    Signed-off-by: Simon Davies <simongdavies@users.noreply.github.com>

--- src/hyperlight_host/src/sandbox/mem_access.rs
+++ src/hyperlight_host/src/sandbox/mem_access.rs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#[cfg(gdb)]
 use std::sync::{Arc, Mutex};
 
 use tracing::{Span, instrument};
@@ -22,29 +23,16 @@ use super::mem_mgr::MemMgrWrapper;
 use crate::error::HyperlightError::StackOverflow;
 #[cfg(gdb)]
 use crate::hypervisor::handlers::{DbgMemAccessHandlerCaller, DbgMemAccessHandlerWrapper};
-use crate::hypervisor::handlers::{
-    MemAccessHandler, MemAccessHandlerFunction, MemAccessHandlerWrapper,
-};
 use crate::mem::shared_mem::HostSharedMemory;
 use crate::{Result, log_then_return};
 
 #[instrument(err(Debug), skip_all, parent = Span::current(), level= "Trace")]
-pub(super) fn handle_mem_access_impl(wrapper: &MemMgrWrapper<HostSharedMemory>) -> Result<()> {
+pub(crate) fn handle_mem_access(wrapper: &MemMgrWrapper<HostSharedMemory>) -> Result<()> {
     if !wrapper.check_stack_guard()? {
         log_then_return!(StackOverflow());
     }
 
     Ok(())
-}
-
-#[instrument(skip_all, parent = Span::current(), level= "Trace")]
-pub(crate) fn mem_access_handler_wrapper(
-    wrapper: MemMgrWrapper<HostSharedMemory>,
-) -> MemAccessHandlerWrapper {
-    let mem_access_func: MemAccessHandlerFunction =
-        Box::new(move || handle_mem_access_impl(&wrapper));
-    let mem_access_hdl = MemAccessHandler::from(mem_access_func);
-    Arc::new(Mutex::new(mem_access_hdl))
 }
 
 #[cfg(gdb)]
